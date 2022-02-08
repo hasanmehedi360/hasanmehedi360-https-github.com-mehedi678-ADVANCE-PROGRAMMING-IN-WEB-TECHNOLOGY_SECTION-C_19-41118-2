b6afccde6d066a45c5041e0d84b187cbe507b2fc
mehedi678 <mhe360999@gmail.com>
2022-02-08 18:49:00 +0600

    APWT_TASK2

---
+++ APWT TASK_2/resources/views/inc/topnav.blade.php
@@ -0,0 +1,6 @@
+
+<a class="btn btn-primary" href="{{route('loginCreate')}}">login</a>
+<a class="btn btn-primary" href="{{route('registrationCreate')}}">Registration</a>
+<a class="btn btn-primary" href="{{route('contactCreate')}}">Contact</a>
+
+
